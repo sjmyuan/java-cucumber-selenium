3f2b6a7ed494ddc88dd96a49508c50363191d583
jiaming.shang <jiaming.shang@rea-group.com>
2017-07-26 21:04:59 +0800

    fix runtime error.

--- src/test/java/cucumber/examples/java/websockets/ServerHooks.java
+++ src/test/java/cucumber/examples/java/websockets/ServerHooks.java
@@ -2,13 +2,36 @@ package cucumber.examples.java.websockets;
 
 import cucumber.api.java8.En;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
 public class ServerHooks implements En {
     static final int PORT = 8887;
 
     private TemperatureServer temperatureServer;
 
     public ServerHooks(){
-        Before(() -> temperatureServer = new TemperatureServer(PORT).start());
-        After(() -> temperatureServer.stop());
+        Before(() -> {
+            try {
+                temperatureServer = new TemperatureServer(PORT).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        After(() -> {
+            try {
+                temperatureServer.stop();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
